5f486e17b3718921f5df1faa14f11ddc4824f220
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2025-03-18 09:12:16 -0400

    saving example code

---
+++ code.py
@@ -0,0 +1,23 @@
+# Created by: Michael Bruneau
+# Created on: March 2025
+#
+# This module is a Raspberrypy Pico program that sends power to RGB LED to create a color then pauses for one second and switches to the next colour and then loops after all colors have been produced
+
+import time
+import board
+import pwmio
+from adafruit_motor import servo
+
+# create a PWMOut object on Pin A2.
+pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
+
+# Create a servo object, my_servo.
+my_servo = servo.Servo(pwm)
+
+while True:
+    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
+        my_servo.angle = angle
+        time.sleep(0.05)
+    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
+        my_servo.angle = angle
+        time.sleep(0.05)
