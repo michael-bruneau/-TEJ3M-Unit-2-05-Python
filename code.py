e29ad63fca8db933fdf777f426b65cb8dcffa4e2
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2025-03-18 09:17:39 -0400

    time to test

--- code.py
+++ code.py
@@ -8,8 +8,11 @@ import board
 import pwmio
 from adafruit_motor import servo
 
+# variables
+blink_delay = 0.05
+
 # create a PWMOut object on Pin A2.
-pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
+pwm = pwmio.PWMOut(board.GP13 , duty_cycle=2 ** 15, frequency=50)
 
 # Create a servo object, my_servo.
 my_servo = servo.Servo(pwm)
@@ -17,7 +20,7 @@ my_servo = servo.Servo(pwm)
 while True:
     for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
         my_servo.angle = angle
-        time.sleep(0.05)
+        time.sleep(blink_delay)
     for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
         my_servo.angle = angle
-        time.sleep(0.05)
+        time.sleep(blink_delay)
